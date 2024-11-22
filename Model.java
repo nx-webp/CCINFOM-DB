57fa42c1ec6219428bdf8e670aa9e734a3e8222a
JohnathanTantanan <164973394+JohnathanTantanan@users.noreply.github.com>
2024-11-22 23:38:34 +0800

    Update Model.java
    -debugged createFlight

--- Model.java
+++ Model.java
@@ -98,20 +98,19 @@ public class Model {
     
     //flight functions
     
-    public boolean createFlight(int flight_id, int gate_number, String destination, String origin, String departure, String arrival, int pilot_id, int copilot_id, int lead_attendant_id, int flight_attendant_id, double price) throws SQLException {
-	String query = "insert into flights (flight_id, gate_number, destination, origin, departure, arrival, pilot_id, copilot_id, lead_attendant, flight_attendant) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);";
+    public boolean createFlight(int gate_number, String destination, String origin, String departure, String arrival, int pilot_id, int copilot_id, int lead_attendant_id, int flight_attendant_id, double price) throws SQLException {
+	String query = "insert into flights (gate_number, destination, origin, departure, arrival, pilot_id, copilot_id, lead_attendant, flight_attendant) values (?, ?, ?, ?, ?, ?, ?, ?, ?);";
 	PreparedStatement stmt = connection.prepareStatement(query);
 
-	stmt.setInt(1, flight_id);
-	stmt.setInt(2, gate_number);
-	stmt.setString(3, destination);
-	stmt.setString(4, origin);
-	stmt.setString(5, departure);
-        stmt.setString(6, arrival);
-        stmt.setInt(7, pilot_id);
-	stmt.setInt(8, copilot_id);
-	stmt.setInt(9, lead_attendant_id);
-	stmt.setInt(10, flight_attendant_id);
+	stmt.setInt(1, gate_number);
+	stmt.setString(2, destination);
+	stmt.setString(3, origin);
+	stmt.setString(4, departure);
+        stmt.setString(5, arrival);
+        stmt.setInt(6, pilot_id);
+	stmt.setInt(7, copilot_id);
+	stmt.setInt(8, lead_attendant_id);
+	stmt.setInt(9, flight_attendant_id);
 	stmt.executeUpdate();
         
         Flight newFlight = new Flight(flights.get(flights.size() - 1).getFlight_id() + 1, price);
