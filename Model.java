2c02384c286539dd3aa5dc1224fae67b00063201
JohnathanTantanan <164973394+JohnathanTantanan@users.noreply.github.com>
2024-11-22 17:09:12 +0800

    Update Model.java
    -added createFlight function

--- Model.java
+++ Model.java
@@ -116,6 +116,20 @@ public class Model {
         return true;
     }
 
+    public boolean createFlight(int flight_id, int pilot_id, int copilot_id, int lead_attendant_id, int flight_attendant_id) throws SQLException {
+	String query = "insert into flights (flight_id, pilot_id, copilot_id, lead_attendant, flight_attendant) values (?, ?, ?, ?, ?);";
+	PreparedStatement stmt = connection.prepareStatement(query);
+
+	stmt.setInt(1, flight_id);
+	stmt.setInt(2, pilot_id);
+	stmt.setInt(3, copilot_id);
+	stmt.setInt(4, lead_attendant_id);
+	stmt.setInt(5, flight_attendant_id);
+        stmt.executeUpdate();
+        return true;
+    }
+	    
+	
     public boolean setCurrentView(String currentView) {
         if (currentView.equals("Home") || currentView.equals("Admin Menu") ||
                 currentView.equals("Passenger Menu") || currentView.equals("Exit Program")) {
